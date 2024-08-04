7fc7822f41f14a83ecde835c1cee77c9efd823bd
Harold Calderon <harold.1408@hotmail.com>
2024-08-03 23:37:50 -0500

    Se Crea logica objetos

--- archive.py
+++ archive.py
@@ -1,10 +1,10 @@
 class Archive:
-    def __init__(self) :
-        pass
-    def create():
-        print("Creando archivo")
-    def list():
+    def __init__(self,name_archive) :
+        self.name_archive=name_archive
+    def create(self):
+        print(f"Creando archivo {self.name_archive}")
+    def list(self):
         print("Listando archivos")
-    def deleate():
-        print("Eliminando archivo")
+    def deleate(self):
+        print(f"Eliminando archivo {self.name_archive}")
         
--- main.py
+++ main.py
@@ -14,9 +14,23 @@ def main():
             option = int(input("Ingresa el número de la opción: "))
         except ValueError:
             print("Por favor, ingresa un número válido.")
-        
-
     
+    match option:
+        case 1:
+            name_archive = input("Opción 1 Crear un archivo \n Ingrese el nombre con extensión del archivo a crear: \n ->")
+            archive_one=Archive(name_archive)
+            archive_one.create()
+        case 2:
+            print("Opción 2 Listar archivos \n Listando espere:")
+            archive_one=Archive("")
+            archive_one.list()
+        case 3:
+            name_archive = input("Opción 3 Eliminar un archivo \n Ingrese el nombre con extensión del archivo a eliminar: \n ->")
+            archive_one=Archive(name_archive)
+            archive_one.deleate()
+        case _:
+            print("Opción 4 salir Vuelva pronto")
+
 
 if __name__ == "__main__":
     main()
